2b56faecf8c689c3beda233d1c6cf793ea16d418
ysirman <24975537+ysirman@users.noreply.github.com>
2022-06-26 14:28:29 +0900

    OAuth2.0に変更

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,8 +5,9 @@ import axios from 'axios'
 export default NextAuth({
   providers: [
     TwitterProvider({
-      clientId: process.env.TWITTER_CONSUMER_KEY ?? '',
-      clientSecret: process.env.TWITTER_CONSUMER_SECRET ?? '',
+      clientId: process.env.TWITTER_CLIENT_ID ?? '',
+      clientSecret: process.env.TWITTER_CLIENT_SECRET ?? '',
+      version: '2.0', // opt-in to Twitter OAuth 2.0
     }),
   ],
   session: {
@@ -16,11 +17,9 @@ export default NextAuth({
     async signIn({ user, _account, profile }) {
       const twitterUser = {
         provider: 'twitter',
-        uid: profile.id,
-        name: profile.name,
-        nickname: profile.screen_name,
-        description: profile.description,
-        location: profile.location,
+        uid: profile.data.id,
+        name: profile.data.username,
+        nickname: profile.data.name,
         image: user.image,
       }
       // バックエンドからJWTを取得する
@@ -29,8 +28,8 @@ export default NextAuth({
           `${process.env.BACKEND_URL}/v1/auth/sign_in`,
           {
             api_key: process.env.BACKEND_API_KEY,
-            email: `twitter${profile.id}@example.com`,
-            password: `password-for-${profile.id}`,
+            email: `twitter${profile.data.id}@example.com`,
+            password: `password-for-${profile.data.id}`,
             ...twitterUser,
           },
           {
