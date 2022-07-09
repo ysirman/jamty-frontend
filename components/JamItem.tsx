605e7df19b573d3357e510da9772137e854ff3d6
ysirman <24975537+ysirman@users.noreply.github.com>
2022-07-09 23:26:42 +0900

    ジャムセッションを中止可能にする

--- components/JamItem.tsx
+++ components/JamItem.tsx
@@ -1,15 +1,23 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, useMutation } from '@apollo/client'
 import { NextPage } from 'next'
 import { JAM_QUERY, JamData } from '../graphql/queries/jam.query'
+import { useState } from 'react'
+import {
+  CANCEL_JAM,
+  CancelJamData,
+} from '../graphql/mutations/cancel-jam.mutation'
 
 interface JamItemProps {
   id: string
 }
 
 const JamItem: NextPage<JamItemProps> = ({ id }) => {
+  const [message, setMessage] = useState<string>('')
   const { loading, error, data } = useQuery<JamData>(JAM_QUERY, {
     variables: { id: parseInt(id) },
   })
+  const [cancelJam, mutationResult] = useMutation<CancelJamData>(CANCEL_JAM)
+  const mutationError = mutationResult.error
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {JSON.stringify(error)}</p>
@@ -17,17 +25,35 @@ const JamItem: NextPage<JamItemProps> = ({ id }) => {
   const { jam } = data
   if (!jam) return null
 
+  const handleCancel = async () => {
+    await cancelJam({
+      variables: {
+        id: parseInt(jam.id),
+      },
+    })
+
+    mutationError
+      ? alert(`Error: ${JSON.stringify(mutationError)}`)
+      : setMessage('Successfully canceled')
+  }
+
   return (
-    <ul>
-      <li>{jam.id}</li>
-      <li>{jam.scheduledFor}</li>
-      <li>{jam.prefectureId}</li>
-      <li>{jam.place}</li>
-      <li>{jam.description}</li>
-      <li>{jam.canceledAt}</li>
-      <li>{jam.createdAt}</li>
-      <li>{jam.updatedAt}</li>
-    </ul>
+    <>
+      <div>
+        <button onClick={handleCancel}>Cancel</button>
+        {message ? <span className="message">{message}</span> : null}
+      </div>
+      <ul>
+        <li>{jam.id}</li>
+        <li>{jam.scheduledFor}</li>
+        <li>{jam.prefectureId}</li>
+        <li>{jam.place}</li>
+        <li>{jam.description}</li>
+        <li>{jam.canceledAt}</li>
+        <li>{jam.createdAt}</li>
+        <li>{jam.updatedAt}</li>
+      </ul>
+    </>
   )
 }
 
